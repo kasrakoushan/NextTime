2a73739ade37e15ccc8fd0bba415399a23ae71f1
Kasra Koushan <kasra.koushan@gmail.com>
2016-06-20 14:40:01 +0200

    Initial commit, including .gitignore

---
+++ NextTimeReminders/LandingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LandingViewController.swift
+//  NextTimeReminders
+//
+//  Created by Kasra Koushan on 2016-06-20.
+//  Copyright © 2016 Kasra Koushan. All rights reserved.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
